f815bf0d1d1078046cd1cb6f66c2e762a5eb833b
Alessandro Longhi <aalelonghi@apollo.tnw.tudelft.nl>
2025-10-13 18:25:57 +0200

    defined models in testing, now need to define dataset

--- src/models/AE_NODE/testing/model_test.py
+++ src/models/AE_NODE/testing/model_test.py
@@ -1,16 +1,70 @@
 import torch as tc
 import os
 import sys
+import pickle
 from ..training.architecture import Encoder, Decoder, F_Latent, Fully_Connected_Encoder, Convolutional_Encoder
-
+from src.common_functions import load_config
 
 class Model_Test:
     def __init__(self , information: dict):
         
         self.path_to_test_data = information['path_to_test_data']
+        self.name_test_file = information['name_test_file']
         self.path_to_model = information['path_to_model']
+        
         directory_images = self.path_to_model+'/Images/'
         os.makedirs(directory_images, exist_ok=True)
+        self.device = information['device']
+        
+        #get normalization
+        
+        with open(self.path_to_test_data + '/maxima_or_mean.pkl', 'rb') as file:
+            self.maxima_or_mean = pickle.load(file)
+        
+        with open(self.path_to_test_data + '/minima_or_std.pkl', 'rb') as file:
+            self.minima_or_std = pickle.load(file)
+        
+        print('Minima or std', self.minima_or_std)
+        print('Maxima or mean', self.maxima_or_mean)
+        
+        self.models_information = load_config(self.path_to_model + 'scripts/configs/configs_models/config_AE_NODE.yaml')
+        self.config_training = load_config(self.path_to_model + 'scripts/configs/config_training.yaml')
+        
+        #define models and load saved checkpoint  
+        self.encoder = Encoder(self.config_training, self.models_information)
+        self.f = F_Latent(self.config_training, self.models_information)
+        self.decoder = Decoder(self.config_training, self.models_information)
+        self.load_checkpoint_on_models()
+        
+        #
+        
+        
+    def load_checkpoint_on_models(self):
+        checkpoint = tc.load(self.path_to_model+'/checkpoint/check.pt', map_location=self.device, weights_only=False)
+        
+        self.encoder.load_state_dict(checkpoint['enco'])
+        self.f.load_state_dict(checkpoint['f'])
+        self.decoder.load_state_dict(checkpoint['dec'])
+
+        total_params_enc = sum(p.numel() for p in self.encoder.parameters() if p.requires_grad)
+        total_params_dec = sum(p.numel() for p in self.decoder.parameters() if p.requires_grad)
+        total_params_f = sum(p.numel() for p in self.f.parameters() if p.requires_grad)
+
+        memory_in_mb = (total_params_enc+total_params_dec+total_params_f * 4) / (1024 ** 2)
+        print(f"Model memory: {memory_in_mb:.2f} MB")
+
+        print(f"Total number of parameters enc: {total_params_enc}")
+        print(f"Total number of parameters dec : {total_params_dec}")
+        print(f"Total number of parameters input f: {total_params_f}")
+        print(f"Total number of parameters: {total_params_enc+total_params_dec+total_params_f}")
+
+        self.encoder.to(self.device)
+        self.f.to(self.device)
+        self.decoder.to(self.device)
+
+        self.encoder.eval()
+        self.f.eval()
+        self.decoder.eval()
         
     def test(self):
         return 0
--- src/models/AE_NODE/testing/support_functions.py
+++ src/models/AE_NODE/testing/support_functions.py
@@ -1,6 +1,1 @@
 import yaml
-
-
-def load_config(config_path: str):
-    with open(config_path, 'r') as file:
-        return yaml.safe_load(file)
