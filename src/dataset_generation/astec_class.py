fixed many issues in dataset, specifically on the order of normalization and substitution of NaN. Now all NaN values are put to zero prior to normalization. removed from dataset values that are always NaN

src/dataset_generation/astec_class.py
@@ -15,63 +15,82 @@ class Astec_Dataset():
         
     def build_training_dataset(self, indeces, purpose_of_data):
         self.purpose_of_data = purpose_of_data
-        self.dictionary_per_time_lengths = extract_input_output_bc_variables(self.path_to_hdf5, indeces, False) #build dictionary of data divided by lenghts of time series
-        self.dictionary_per_time_lengths = self.make_channels_for_dictionary_per_time_lengths(self.dictionary_per_time_lengths) #build dictionary of data divided by lenghts of time series and make channels per spatial domain
-                
-        #save dictionary_per_time_lengths to hdf5s if self.save_dictionary_per_time_lengths is true
+        self.dictionary_per_simulation = extract_input_output_bc_variables(self.path_to_hdf5, indeces) #build dictionary of data divided by number of simulation
+        self.dictionary_per_simulation = self.make_channels_for_dictionary_per_simulation(self.dictionary_per_simulation) #build dictionary of data divided by simulations and make channels per spatial domain
+        self.dictionary_per_simulation = self.substitute_NaN_with_zeros(self.dictionary_per_simulation)
+        
+        #save dictionary_per_simulation to hdf5s if self.save_dictionary_per_time_lengths is true
         if self.save_dictionary_per_time_lengths:
             with h5py.File(self.path_to_hdf5+'/data_'+self.purpose_of_data+'.h5', 'w') as f:
-                dict_to_hdf5(self.dictionary_per_time_lengths, f) 
+                dict_to_hdf5(self.dictionary_per_simulation, f) 
         
-        # get unified dataset to get the normalizations statistics
-        dictionary_unified = self.make_dictionary_unified()
         #get normalization statistics
-        self.maxima_or_mean, self.minima_or_std = get_normalization_statistics(dictionary_unified, self.which_normalization) 
-        for key in self.maxima_or_mean:
-            print(f'maxima_or_mean {key}, ',self.maxima_or_mean[key])
-        print(' ')
-        for key in self.minima_or_std:
-            print(f'minima_or_std {key}, ',self.minima_or_std[key])
+        if purpose_of_data == 'training': 
+            # get unified dataset to get the normalizations statistics
+            dictionary_unified = self.make_dictionary_unified()
+            self.maxima_or_mean, self.minima_or_std = get_normalization_statistics(dictionary_unified, self.which_normalization) 
             
-        self.maxima_or_mean = {k: np.float32(v) for k, v in self.maxima_or_mean.items()}
-        self.minima_or_std = {k: np.float32(v) for k, v in self.minima_or_std.items()}
+            for key in self.maxima_or_mean:
+                print(f'maxima_or_mean {key}, ',self.maxima_or_mean[key])
                 
-        #divide datasets in time windows of length t_W and mix the trajectories
+            print(' ')
+            
+            for key in self.minima_or_std:
+                print(f'minima_or_std {key}, ',self.minima_or_std[key])
+            
+            self.maxima_or_mean = {k: np.float32(v) for k, v in self.maxima_or_mean.items()}
+            self.minima_or_std = {k: np.float32(v) for k, v in self.minima_or_std.items()}
+            
+            #save normalization statistics for training and testing
+            with open(self.path_to_hdf5+'/maxima_or_mean.pkl', 'wb') as f:
+                pickle.dump(self.maxima_or_mean, f)
+
+            with open(self.path_to_hdf5+'/minima_or_std.pkl', 'wb') as f:
+                pickle.dump(self.minima_or_std, f)
+                
+        elif purpose_of_data == 'validation':
+            
+            with open(self.path_to_hdf5 + '/maxima_or_mean.pkl', 'rb') as file:
+                self.maxima_or_mean = pickle.load(file)
+            
+            with open(self.path_to_hdf5 + '/minima_or_std.pkl', 'rb') as file:
+                self.minima_or_std = pickle.load(file)
+            
+        #divide datasets in time windows of length t_W, pad and mix the trajectories
         self.dictionary_of_sliced_windows = self.make_dictionary_of_sliced_windows()
 
-        #save normalization statistics for training and testing
-        with open(self.path_to_hdf5+'/maxima_or_mean.pkl', 'wb') as f:
-            pickle.dump(self.maxima_or_mean, f)
-
-        with open(self.path_to_hdf5+'/minima_or_std.pkl', 'wb') as f:
-            pickle.dump(self.minima_or_std, f)
-        
-        #normalize dictionary with sliced windows
+        #normalize dictionary with sliced windows and reshape in correct shapes
         self.dictionary_of_sliced_windows = self.normalize_dictionary_of_sliced_windows()
         
         #check if there are nan values in the dictionary before saving
         for key in self.dictionary_of_sliced_windows:
             if np.isnan(self.dictionary_of_sliced_windows[key]).any() or not np.isfinite(self.dictionary_of_sliced_windows[key]).any():
                 raise TypeError(f"There are still NaN values in final data, check please in {key}")
+        #check shapes
+        print('')
+        print('CHECK SHAPES PLEASE!')  
+        print('')
+        for key in self.dictionary_of_sliced_windows:
+            print(key, np.shape(self.dictionary_of_sliced_windows[key]))
         #save normalized dictionary with sliced windows
         with h5py.File(f'{self.path_to_hdf5}/data_{self.purpose_of_data}_normalized_t_W_500.h5', 'w') as f:
             dict_to_hdf5(self.dictionary_of_sliced_windows, f)
             
         return 0
     
-    def make_channels_for_dictionary_per_time_lengths(self, dict:dict):
-        for l_t_s in dict: #l_t_s is length time series
-            for m_t in dict[l_t_s]: #m_t is the mesh type (so 1, 76, 32 and so on )
-                fields = list(dict[l_t_s][m_t].keys())
-                size_field = len(np.shape(dict[l_t_s][m_t][fields[0]])) #final [0] to take first batch in case there are multiple trajectories with the same time lenght
+    def make_channels_for_dictionary_per_simulation(self, dict:dict):
+        for n_o_s in dict: #n_o_s is number of simulation
+            for m_t in dict[n_o_s]: #m_t is the mesh type (so 1, 76, 32 and so on )
+                fields = list(dict[n_o_s][m_t].keys())
+                size_field = len(np.shape(dict[n_o_s][m_t][fields[0]])) #final [0] to take first batch in case there are multiple trajectories with the same time lenght
                 if  size_field == 3:
-                    concatenated_array = np.concatenate([np.array(dict[l_t_s][m_t][field])[:,:,None,:] for field in dict[l_t_s][m_t]], axis = 2)
+                    concatenated_array = np.concatenate([np.array(dict[n_o_s][m_t][field])[:,:,None,:] for field in dict[n_o_s][m_t]], axis = 2)
                 elif size_field == 2:
-                    concatenated_array = np.concatenate([np.array(dict[l_t_s][m_t][field])[:,:, None] for field in dict[l_t_s][m_t]], axis = -1)
+                    concatenated_array = np.concatenate([np.array(dict[n_o_s][m_t][field])[:,:, None] for field in dict[n_o_s][m_t]], axis = -1)
                 else:
                     raise TypeError("Something is wrong with data structure")   
                  
-                dict[l_t_s][m_t] = concatenated_array
+                dict[n_o_s][m_t] = concatenated_array
         # now mix the bcs
         for i in dict:
             dict[i]['boundary_conditions_and_time'] = np.concatenate([dict[i]['primary_to_vessel'], dict[i]['vessel_to_primary']],axis = -1)
@@ -81,32 +100,33 @@ class Astec_Dataset():
         return dict
     
     def make_dictionary_unified(self):
-        keys = list(self.dictionary_per_time_lengths.keys())
+        numbers_of_simulation = list(self.dictionary_per_simulation.keys())
         dictionary_unified = {}
-        variables = self.dictionary_per_time_lengths[keys[0]].keys()
-        for key in keys:
-            for x in variables:
-                size = np.shape(self.dictionary_per_time_lengths[key][x])
-                reshaped_field = np.reshape(self.dictionary_per_time_lengths[key][x], (size[0] * size[1],)+size[2:])
-                dictionary_unified[x] = reshaped_field
+        variables = self.dictionary_per_simulation[numbers_of_simulation[0]].keys()
+        for numbers_of_simulation in numbers_of_simulation:
+            for variable in variables:
+                size = np.shape(self.dictionary_per_simulation[numbers_of_simulation][variable])
+                reshaped_field = np.reshape(self.dictionary_per_simulation[numbers_of_simulation][variable], (size[0] * size[1],)+size[2:])
+                dictionary_unified[variable] = reshaped_field
                 
         return dictionary_unified
             
     def make_dictionary_of_sliced_windows(self):
-        keys = list(self.dictionary_per_time_lengths.keys())
+        numbers_of_simulation = list(self.dictionary_per_simulation.keys())
         dictionary_of_sliced_windows = {}
-        variables = self.dictionary_per_time_lengths[keys[0]].keys()
-        for x in variables:
-            size = (1, self.t_W) + np.shape(self.dictionary_per_time_lengths[keys[0]][x])[2:] 
-            dictionary_of_sliced_windows[x] = tc.tensor([])
+        variables = self.dictionary_per_simulation[numbers_of_simulation[0]].keys()
+        for variable in variables:
+            size = (1, self.t_W) + np.shape(self.dictionary_per_simulation[numbers_of_simulation[0]][variable])[2:] 
+            dictionary_of_sliced_windows[variable] = tc.tensor([])
         dictionary_of_sliced_windows['length_of_padding'] = tc.tensor([])
-        for key in keys:
-            for x in variables:
-                size = np.shape(self.dictionary_per_time_lengths[key][x])
+        
+        for number_of_simulation in numbers_of_simulation:
+            for variable in variables:
+                size = np.shape(self.dictionary_per_simulation[number_of_simulation][variable])
                 pad_to_be_added = (self.t_W - (size[1]%self.t_W)) % self.t_W
-                padded_tensor = tc.nn.functional.pad(tc.tensor(np.array(self.dictionary_per_time_lengths[key][x])), (0, 0)*(len(size)-2) + (0, pad_to_be_added))
+                padded_tensor = tc.nn.functional.pad(tc.tensor(np.array(self.dictionary_per_simulation[number_of_simulation][variable])), (0, 0)*(len(size)-2) + (0, pad_to_be_added))
                 padded_tensor = tc.reshape(padded_tensor, (size[0] * int((size[1]+pad_to_be_added)/self.t_W), self.t_W) + size[2:])
-                dictionary_of_sliced_windows[x] = tc.concatenate((dictionary_of_sliced_windows[x], padded_tensor), axis = 0)
+                dictionary_of_sliced_windows[variable] = tc.concatenate((dictionary_of_sliced_windows[variable], padded_tensor), axis = 0)
                 padding_tracker = tc.zeros(int((size[1]+pad_to_be_added)/self.t_W))
             padding_tracker[-1] = pad_to_be_added
             dictionary_of_sliced_windows['length_of_padding'] = tc.concatenate((dictionary_of_sliced_windows['length_of_padding'], padding_tracker), axis = 0)
@@ -116,10 +136,10 @@ class Astec_Dataset():
     def normalize_dictionary_of_sliced_windows(self):
         
         normalized_dict = {}
-        keys = self.dictionary_of_sliced_windows.keys()
-        for key in keys:
-            if key != 'length_of_padding':
-                original_data = self.dictionary_of_sliced_windows[key][:]
+        variables = self.dictionary_of_sliced_windows.keys()
+        for variable in variables:
+            if variable != 'length_of_padding':
+                original_data = self.dictionary_of_sliced_windows[variable][:]
                 normalized_data = normalize_fields(original_data, self.maxima_or_mean, self.minima_or_std, self.which_normalization)
                 shape = np.shape(normalized_data)
                 if shape[-1] == 140:
@@ -133,22 +153,62 @@ class Astec_Dataset():
                     normalized_dict['lower_plenum'] = lower_plenum
                 elif shape[-1] == 7 or shape[-1] == 13 and len(shape) !=2:
                     normalized_data = np.reshape(normalized_data, (shape[0],shape[1],shape[2]))
-                normalized_dict[key] = normalized_data
+                normalized_dict[variable] = normalized_data
             else:
-                normalized_dict[key] = self.dictionary_of_sliced_windows[key]
+                normalized_dict[variable] = self.dictionary_of_sliced_windows[variable]
                 
         return normalized_dict
     
+    def substitute_NaN_with_zeros(self, dictionary):
+        trajectories = list(dictionary.keys())
+        shapes = dictionary[trajectories[0]].keys()
+        for trajectory in trajectories:
+            for shape in shapes:
+                dictionary[trajectory][shape] = np.nan_to_num(dictionary[trajectory][shape], nan=0.0)
+        return dictionary
+                
     def build_testing_dataset(self, indeces):
         
-        dictionary_per_trajectory = extract_input_output_bc_variables(self.path_to_hdf5, indeces, True) #build dictionary of data divided by lenghts of time series
-        dictionary_per_trajectory = self.make_channels_for_dictionary_per_time_lengths(dictionary_per_trajectory) #build dictionary of data divided by lenghts of time series and make channels per spatial domain
+        with open(self.path_to_hdf5 + '/maxima_or_mean.pkl', 'rb') as file:
+            maxima_or_mean = pickle.load(file)
+        
+        with open(self.path_to_hdf5 + '/minima_or_std.pkl', 'rb') as file:
+            minima_or_std = pickle.load(file)
+        
+        dictionary_per_trajectory = extract_input_output_bc_variables(self.path_to_hdf5, indeces) #build dictionary of data divided by numbers of simulations
+        dictionary_per_trajectory = self.make_channels_for_dictionary_per_simulation(dictionary_per_trajectory) #build dictionary of data divided by numbers of simulation and make channels per spatial domain
+        dictionary_per_trajectory = self.substitute_NaN_with_zeros(dictionary_per_trajectory)
+        dictionary_per_trajectory = self.normalize_testing_dataset(dictionary_per_trajectory, maxima_or_mean, minima_or_std) #normalize testing dataset according to training statistics
         dictionary_per_trajectory = self.reshape_testing_dataset(dictionary_per_trajectory) #reshape dictionary
-                
-        #save dictionary_per_time_lengths to hdf5s if self.save_dictionary_per_time_lengths is true
+        
+        #check if there are nan values in the dictionary before saving
+        for number_of_simulation in dictionary_per_trajectory:
+            shapes = list(dictionary_per_trajectory[number_of_simulation].keys())
+            for shape in shapes:
+                if np.isnan(dictionary_per_trajectory[number_of_simulation][shape]).any() or not np.isfinite(dictionary_per_trajectory[number_of_simulation][shape]).any():
+                    raise TypeError(f"There are still NaN values in final data, check please in simulation {number_of_simulation}, shape {shape}")
+            
+        #check shapes
+        print('')
+        print('CHECK SHAPES PLEASE!')  
+        print('')
+        for number_of_simulation in dictionary_per_trajectory:
+            shapes = list(dictionary_per_trajectory[number_of_simulation].keys())
+            for shape in shapes:
+                print(f"shape {number_of_simulation}, {shape}", np.shape(dictionary_per_trajectory[number_of_simulation][shape]))
+            
+        #save dictionary_per_simulation to hdf5s if self.save_dictionary_per_time_lengths is true
         with h5py.File(self.path_to_hdf5+'/data_testing.h5', 'w') as f:
             dict_to_hdf5(dictionary_per_trajectory, f) 
-            
+    
+    def normalize_testing_dataset(self, dictionary_per_trajectory:dict, maxima_or_mean : dict, minima_or_std: dict):
+        normalized_dict = {}
+        for number_of_simulation in dictionary_per_trajectory:
+            for variable in dictionary_per_trajectory[number_of_simulation]:
+                normalized_field = normalize_fields(tc.tensor(dictionary_per_trajectory[number_of_simulation][variable]), maxima_or_mean, minima_or_std, self.which_normalization)
+                normalized_dict.setdefault(number_of_simulation, {})[variable] = normalized_field
+        return normalized_dict
+        
     def reshape_testing_dataset(self, dictionary_per_trajectory):
         reshaped_dict = {}
         trajectories = dictionary_per_trajectory.keys()

src/dataset_generation/support_functions.py
@@ -70,8 +70,6 @@ def build_dictionary_of_variables():
                             
                             'm_cum_H2': [],
                             'm_tot_cor': [],
-                            
-                            'm_tot_deb': [],
                             'FP_A_heat': [],
                             'sat_core_mesh': []
                         },
@@ -95,7 +93,7 @@ def fill_dictionary_of_variables(output_dict:dict, name:str, f:h5py._hl.files.Fi
     # Existing variables
     output_dict[name]['dictionary_of_input_variables_1']['m_cum_H2'].append(np.array(f['vessel/general/m_cum_H2'][:])[0:index_stop])
     output_dict[name]['dictionary_of_input_variables_1']['m_tot_cor'].append(np.array(f['vessel/general/m_tot_cor'][:])[0:index_stop])
-    output_dict[name]['dictionary_of_input_variables_1']['m_tot_deb'].append(np.array(f['vessel/general/m_tot_deb'][:])[0:index_stop])
+    #output_dict[name]['dictionary_of_input_variables_1']['m_tot_deb'].append(np.array(f['vessel/general/m_tot_deb'][:])[0:index_stop]) #it is always nan for some reason
     #output_dict[index_stop]['dictionary_of_input_variables_1']['T_CAVCOR'].append(np.array(f['other/global/sensor_values'][:, 286])[0:index_stop]) #this changes only after vessel rupture
     
     
@@ -175,7 +173,7 @@ def fill_dictionary_of_variables(output_dict:dict, name:str, f:h5py._hl.files.Fi
     
     output_dict[name]['vessel_to_primary']['time'].append(DT)
 
-def extract_input_output_bc_variables(path, array_of_datasets:list, testing:bool):
+def extract_input_output_bc_variables(path, array_of_datasets:list):
     output_dict = {}
     for i in array_of_datasets:
         with h5py.File(path+'/'+str(i)+'.h5', 'r') as f:
@@ -186,13 +184,9 @@ def extract_input_output_bc_variables(path, array_of_datasets:list, testing:bool
             else:
                 index_stop = len(f['dimensions/time_points'][:])
                 
-            if not testing:
-                if index_stop not in output_dict:
-                    output_dict[index_stop] = build_dictionary_of_variables()
-                fill_dictionary_of_variables(output_dict, index_stop, f, index_stop)
-            else:
-                output_dict[i] = build_dictionary_of_variables()
-                fill_dictionary_of_variables(output_dict, i, f, index_stop)
+            output_dict[i] = build_dictionary_of_variables()
+            fill_dictionary_of_variables(output_dict, i, f, index_stop)
+                
             
     return output_dict
 
@@ -217,12 +211,6 @@ def get_normalization_statistics(dictionary_of_sliced_windows:dict, type_of_norm
             maximum = np.nanmax(dictionary_of_sliced_windows[key].astype(np.float64),axis = (0,) + tuple(np.arange(2,len(shape))))
             minima_or_std[key] = minimum
             maxima_or_mean[key] = maximum
-
-        for key in maxima_or_mean:
-            for count, i in enumerate(maxima_or_mean[key]):
-                if i == minima_or_std[key][count] or np.isnan(maxima_or_mean[key][count]): #for when all the values are NaN (but padding makes normalization see zeros...)
-                    maxima_or_mean[key][count] = 1.0
-                    minima_or_std[key][count] = 0.0
                     
     elif type_of_normalization == 'mean_std':
         for key in hdf5_keys:
@@ -232,110 +220,61 @@ def get_normalization_statistics(dictionary_of_sliced_windows:dict, type_of_norm
             minima_or_std[key] = std
             maxima_or_mean[key] = mean
 
-        for key in maxima_or_mean:
-            for count, i in enumerate(maxima_or_mean[key]):
-                if i == minima_or_std[key][count] or np.isnan(maxima_or_mean[key][count]): #for when all the values are NaN (but padding makes normalization see zeros...)
-                    maxima_or_mean[key][count] = 0.0
-                    minima_or_std[key][count] = 1.0
     else:
         raise TypeError("Type of normalization not known. It can either be min_max or mean_std")                 
     return maxima_or_mean, minima_or_std
 
-def normalize_fields(fields: list, ma_means: dict, mi_stds: dict, type_of_normalization: str):
-    size = np.shape(fields)
+def normalize_fields(field: np.array, maximum_or_mean: dict, minimum_or_std: dict, normalization: str):
+    size = np.shape(field)
     
-    if type_of_normalization == 'min_max':
-        if size[-1] == 7:
-            minimum = mi_stds['boundary_conditions_and_time']
-            maximum = ma_means['boundary_conditions_and_time']
-            minimum = minimum[None,None,:]
-            maximum = maximum[None,None,:]
-        elif size[-1] == 5:
-            minimum = mi_stds['dictionary_of_input_variables_1']
-            maximum = ma_means['dictionary_of_input_variables_1']
-            minimum = minimum[None,None,:]
-            maximum = maximum[None,None,:]
-
-        elif size[-1] == 140:
-            minimum = mi_stds['dictionary_of_input_variables_140']
-            maximum = ma_means['dictionary_of_input_variables_140']
-            minimum = minimum[None,None,:, None]
-            maximum = maximum[None,None,:, None]
-
-        elif size[-1] == 36:
-            minimum = mi_stds['dictionary_of_input_variables_36']
-            maximum = ma_means['dictionary_of_input_variables_36']
-            minimum = minimum[None,None,:, None]
-            maximum = maximum[None,None,:, None]
-
-        elif size[-1] == 76:
-            minimum = mi_stds['dictionary_of_input_variables_76']
-            maximum = ma_means['dictionary_of_input_variables_76']
-            minimum = minimum[None,None,:, None]
-            maximum = maximum[None,None,:, None]
-            
-        else:
-            raise TypeError(f"Something is wrong with data")
-     
-        # Create a mask for NaN values - use torch.isnan and ensure bool dtype
-        nan_mask = tc.isnan(fields)
+    if size[-1] == 7:
         
-        # Apply normalization
-        normalized = (fields - minimum) / (maximum - minimum)
+        maximum_or_mean = maximum_or_mean['boundary_conditions_and_time']
+        minimum_or_std = minimum_or_std['boundary_conditions_and_time']
+        maximum_or_mean = maximum_or_mean[None,None,:]
+        minimum_or_std = minimum_or_std[None,None,:]
         
-        #put 0.0 where NaN. It makes sense for m_debris_1_vessel and m_debris_0_vessel
-        normalized = normalized.masked_fill(nan_mask, 0.0)
-        fields = normalized
-        
-    elif type_of_normalization == 'mean_std':
-        if size[-1] == 7:
-            std = mi_stds['boundary_conditions_and_time']
-            mean = ma_means['boundary_conditions_and_time']
-            std = std[None,None,:]
-            mean = mean[None,None,:]
-        elif size[-1] == 5:
-            std = mi_stds['dictionary_of_input_variables_1']
-            mean = ma_means['dictionary_of_input_variables_1']
-            std = std[None,None,:]
-            mean = mean[None,None,:]
+    elif size[-1] == 4:
+        maximum_or_mean = maximum_or_mean['dictionary_of_input_variables_1']
+        minimum_or_std = minimum_or_std['dictionary_of_input_variables_1']
+        maximum_or_mean = maximum_or_mean[None,None,:]
+        minimum_or_std = minimum_or_std[None,None,:]
 
-        elif size[-1] == 140:
-            std = mi_stds['dictionary_of_input_variables_140']
-            mean = ma_means['dictionary_of_input_variables_140']
-            std = std[None,None,:, None]
-            mean = mean[None,None,:, None]
+    elif size[-1] == 140:
+        maximum_or_mean = maximum_or_mean['dictionary_of_input_variables_140']
+        minimum_or_std = minimum_or_std['dictionary_of_input_variables_140']
+        maximum_or_mean = maximum_or_mean[None,None,:, None]
+        minimum_or_std = minimum_or_std[None,None,:, None]
 
-        elif size[-1] == 36:
-            std = mi_stds['dictionary_of_input_variables_36']
-            mean = ma_means['dictionary_of_input_variables_36']
-            std = std[None,None,:, None]
-            mean = mean[None,None,:, None]
+    elif size[-1] == 36:
+        maximum_or_mean = maximum_or_mean['dictionary_of_input_variables_36']
+        minimum_or_std = minimum_or_std['dictionary_of_input_variables_36']
+        maximum_or_mean = maximum_or_mean[None,None,:, None]
+        minimum_or_std = minimum_or_std[None,None,:, None]
 
-        elif size[-1] == 76:
-            std = mi_stds['dictionary_of_input_variables_76']
-            mean = ma_means['dictionary_of_input_variables_76']
-            std = std[None,None,:, None]
-            mean = mean[None,None,:, None]
+    elif size[-1] == 76:
+        maximum_or_mean = maximum_or_mean['dictionary_of_input_variables_76']
+        minimum_or_std = minimum_or_std['dictionary_of_input_variables_76']
+        maximum_or_mean = maximum_or_mean[None,None,:, None]
+        minimum_or_std = minimum_or_std[None,None,:, None]
         
-        else:
-            raise TypeError(f"Something is wrong with data")
-
-        # Create a mask for NaN values - use torch.isnan and ensure bool dtype
-        nan_mask = tc.isnan(fields)
-        
-        # Apply normalization
-        normalized = (fields - mean) / std
-        
-        #put 0.0 where NaN. It makes sense for m_debris_1_vessel and m_debris_0_vessel
-        normalized = normalized.masked_fill(nan_mask, 0.0) 
-        fields = normalized
-            
-    elif type_of_normalization == 'none':
-        return fields
+    else:
+        raise TypeError(f"Something is wrong with data")
     
+    if normalization == 'min_max':
+        
+        field = ((field - minimum_or_std)/(maximum_or_mean - minimum_or_std))
+            
+    elif normalization == 'mean_std':
+        
+        fields = ((field - maximum_or_mean)/minimum_or_std)
+        
+    elif normalization == 'none':
+        return field
+
     else:
         raise ValueError(f"Missing value") 
-    
-    return fields
+        
+    return field
 
 

src/models/AE_NODE/training/architecture.py
@@ -42,12 +42,13 @@ class Encoder(nn.Module):
         latent_faces_variables = self.encoder_faces_variables(faces_variables)
         
         #concatenate latent variables without latent_boundaries_variables
+        latent_in_variables_separated = [latent_scalar_variables, latent_plenum_variables, latent_core_variables, latent_vessel_variables, latent_faces_variables] #useful at testing
         latent_in_variables = tc.concatenate((latent_scalar_variables, latent_plenum_variables, latent_core_variables, latent_vessel_variables, latent_faces_variables), axis = -1)
         regularization_latent = self.l1_latent_regularization(latent_in_variables, self.lambda_regularization, latent_boundaries_variables) #regularization latent space 
-        latent_in_variables = self.final_reduction(latent_in_variables) #final reduced vector of inner fields
+        final_latent = self.final_reduction(latent_in_variables) #final reduced vector of inner fields
         
         
-        return latent_in_variables, latent_boundaries_variables, regularization_latent
+        return final_latent, latent_in_variables_separated, latent_boundaries_variables, regularization_latent
     
     def l1_latent_regularization(self, latent_fields: list, lambda_l1: float, latent_boundaries: tc.tensor = None):
         

src/models/AE_NODE/training/data_functions.py
@@ -3,6 +3,7 @@ import torch as tc
 import numpy as np
 import h5py
 from torch.utils.data import Dataset
+from torch import nn
     
 def save_checkpoint(enco, f , dec, optimizer, scheduler, epoch, loss, loss_coeff_2, start_backprop,full_training_count,filepath):
   
@@ -66,9 +67,8 @@ def standard_and_inverse_normalization_field(x: list, maxima_or_mean: dict, mini
     x_denormalized = []
     
     for _, i in enumerate(x):
-        # Create a mask for values that are not -1
-        mask = (i != -1)
-        if i.size(-1) == 5 and len(i.size()) == 3:
+        
+        if i.size(-1) == 4 and len(i.size()) == 3:
             maximum_or_mean = maxima_or_mean['dictionary_of_input_variables_1'][None,None,:]
             minimum_or_std = minima_or_std['dictionary_of_input_variables_1'][None,None,:]
 
@@ -98,20 +98,87 @@ def standard_and_inverse_normalization_field(x: list, maxima_or_mean: dict, mini
         if normalization == 'min_max':
             if inverse:
                 denorm = (i * (maximum_or_mean - minimum_or_std) + minimum_or_std)
-                x_denormalized.append(tc.where(mask, denorm, i))
+                x_denormalized.append(denorm)
             else:
                 norm = ((i - minimum_or_std)/(maximum_or_mean - minimum_or_std))
-                x_denormalized.append(tc.where(mask, norm, i))
+                x_denormalized.append(denorm)
                 
         elif normalization == 'mean_std':
             if inverse:
                 denorm = (i * minimum_or_std + maximum_or_mean)
-                x_denormalized.append(tc.where(mask, denorm, i))
+                x_denormalized.append(denorm)
             else:
                 norm = ((i - maximum_or_mean)/minimum_or_std)
-                x_denormalized.append(tc.where(mask, norm, i))
+                x_denormalized.append(denorm)
             
     return x_denormalized
+
+def create_padding_mask(size_of_tensor: list, length_of_padding: tc.tensor, device: tc.device):
+        
+        mask = tc.zeros(size_of_tensor, device = device)
+        columns = tc.arange(size_of_tensor[1])
+        where_to_fill = columns>=length_of_padding
+        where_to_fill = where_to_fill[(...,) + (None,) * (len(size_of_tensor)-2)].expand(size_of_tensor).to(device)
+        mask = mask.masked_fill(where_to_fill, 1.0)
+        
+        return mask
+
+
+def MSE(input: list, target: list, length_of_padding: tc.tensor = None, reconstructed_boundaries: tc.tensor = None, boundaries: tc.tensor = None, is_denormalized_validation = False):
+    mse = []
+    device = input[0].device
+    if (length_of_padding is not None) and tc.any(length_of_padding != 0.0):
+        for count, i in enumerate(input):   
+            if is_denormalized_validation:
+                i = i.double()
+                target[count] = target[count].double()
+            loss = nn.MSELoss(reduction='none')
+            element_loss = loss(i, target[count]) 
+            mask = create_padding_mask( size_of_tensor=i.size(), length_of_padding=length_of_padding, device = device) 
+            masked_loss = element_loss * mask
+            if is_denormalized_validation:
+                masked_target = ((target[count] * mask)**2).mean()
+                mse_value = (masked_loss.double()/masked_target.double()).sum() / (mask.sum() * i.size(2))
+                mse.append(mse_value)
+            else:
+                mse_value = masked_loss.sum() / (mask.sum() * i.size(2))
+                mse.append(mse_value)
+    
+        if reconstructed_boundaries is not None:
+            element_loss = loss(reconstructed_boundaries, boundaries) 
+            mask = create_padding_mask( size_of_tensor=reconstructed_boundaries.size(), length_of_padding=length_of_padding, device = device) 
+            masked_loss = element_loss * mask
+            
+            if is_denormalized_validation:
+                masked_target = ((boundaries * mask)**2).mean()
+                mse_value = (masked_loss/masked_target).sum() / (mask.sum() * reconstructed_boundaries.size(2))
+                mse.append(mse_value)
+            else:
+                mse_value = masked_loss.sum() / (mask.sum() * reconstructed_boundaries.size(2))
+                mse.append(mse_value)
+                
+    else:
+        loss = nn.MSELoss()
+        for count, i in enumerate(input):
+            if is_denormalized_validation:
+                i = i.double()
+                target[count] = target[count].double()
+            mse_value = loss(i,target[count])
+            if is_denormalized_validation:
+                mse_value = mse_value/(target[count]**2).mean()
+            mse.append(mse_value)
+                
+        if reconstructed_boundaries is not None:
+            mse_value = loss(reconstructed_boundaries, boundaries)
+            if is_denormalized_validation:
+                mse_value = mse_value/(boundaries**2).mean()
+            mse.append(mse_value) 
+            
+    if is_denormalized_validation:  #for training and testing
+        return tc.stack(mse)
+    
+    elif not is_denormalized_validation: #for training 
+        return tc.stack(mse).mean()
     
 
       

src/models/AE_NODE/training/method_functions.py
@@ -1,5 +1,4 @@
 from src.models.AE_NODE.training.data_functions import *
-from torch import nn
 import time 
 
 class Training_Losses():
@@ -35,7 +34,7 @@ class Training_Losses():
 
     def auto_encoding_loss(self, fields:list , boundaries:tc.tensor,length_of_padding:tc.tensor ,loss_coeff:list, train:bool):
         
-        latent_vector_fields, latent_boundaries, regularization_latent = self.encoder(fields, boundaries)
+        latent_vector_fields, _ , latent_boundaries, regularization_latent = self.encoder(fields, boundaries)
         reconstructed_variables, reconstructed_boundaries = self.decoder(latent_vector_fields, latent_boundaries)
         
         # separate the reconstruction of boundaries and fields
@@ -45,9 +44,9 @@ class Training_Losses():
         reconstructed_boundaries = tc.reshape(reconstructed_boundaries, ((boundaries.size()[0],boundaries.size()[1]) + reconstructed_boundaries.size()[1:]))
 
         if train:
-            l1 = self.MSE(reconstructed_variables, fields, length_of_padding, reconstructed_boundaries, boundaries) * loss_coeff[0]
+            l1 = MSE(reconstructed_variables, fields, length_of_padding, reconstructed_boundaries, boundaries) * loss_coeff[0]
         else:
-            first = self.MSE(reconstructed_variables, fields, length_of_padding) * loss_coeff[0]
+            first = MSE(reconstructed_variables, fields, length_of_padding) * loss_coeff[0]
             
             reconstructed_variables = standard_and_inverse_normalization_field(reconstructed_variables, self.maxima_or_mean, self.minima_or_std, self.which_normalization, True)
             reconstructed_boundaries = standard_and_inverse_normalization_field([reconstructed_boundaries], self.maxima_or_mean, self.minima_or_std, self.which_normalization, True)[0]
@@ -55,7 +54,7 @@ class Training_Losses():
             fields = standard_and_inverse_normalization_field(fields, self.maxima_or_mean, self.minima_or_std, self.which_normalization, True)
             boundaries = standard_and_inverse_normalization_field([boundaries], self.maxima_or_mean, self.minima_or_std, self.which_normalization, True)[0]
 
-            second = self.MSE(reconstructed_variables, fields, length_of_padding, reconstructed_boundaries, boundaries, is_denormalized_validation = True) * loss_coeff[0]
+            second = MSE(reconstructed_variables, fields, length_of_padding, reconstructed_boundaries, boundaries, is_denormalized_validation = True) * loss_coeff[0]
             l1 = [first, second]
             
         return latent_vector_fields, latent_boundaries, l1, regularization_latent
@@ -143,10 +142,10 @@ class Training_Losses():
             l2_AR = tc.tensor(0., device = self.device)
 
             if (number_of_time_steps-1-self.start_backprop[1]) == 0:
-                next_latent, _ , _ = self.encoder(initial_condition)
+                next_latent, _, _ , _ = self.encoder(initial_condition)
             else:
                 with tc.no_grad():
-                    next_latent, _ , _ = self.encoder(initial_condition)
+                    next_latent, _ , _ , _ = self.encoder(initial_condition)
 
             if (not train):
                 fields = standard_and_inverse_normalization_field(fields, self.maxima_or_mean, self.minima_or_std, self.which_normalization, True)
@@ -166,13 +165,13 @@ class Training_Losses():
                     output_decoder, _ = self.decoder(next_latent)
                     denorm_latent = standard_and_inverse_normalization_field(output_decoder, self.maxima_or_mean, self.minima_or_std, self.which_normalization, True)
                     fields_at_correct_time_step = [tensor[:, count+1, ...] for tensor in fields]
-                    l_final += self.MSE(denorm_latent, fields_at_correct_time_step) #the boundary should not be taken into account here
+                    l_final += MSE(denorm_latent, fields_at_correct_time_step) #the boundary should not be taken into account here
             return l2_AR/step * coeff, l_final/(number_of_time_steps-1)
         
         elif self.start_backprop[0] == 1: #Encode ic and evolve in latent but TBPP
 
             place_holder = tc.zeros((number_batches,number_of_time_steps-self.start_backprop[1], true_latent.size()[-1]), device = self.device)
-            next_latent = self.encoder(input_encoder[:,0,...])
+            next_latent, _, _, _ = self.encoder(input_encoder[:,0,...])
             place_holder[:,0,:] = next_latent.clone()
             with tc.no_grad():
                 for i in range(number_of_time_steps-self.start_backprop[1]-1):
@@ -181,7 +180,7 @@ class Training_Losses():
 
             place_holder = tc.reshape(place_holder,(number_batches*(number_of_time_steps-self.start_backprop[1]),true_latent.size()[-1]))
             l2_AR_1 = tc.tensor(0.0,device=self.device)
-            next_latent = self.encoder(input_encoder[:,0,...])
+            next_latent, _, _, _ = self.encoder(input_encoder[:,0,...])
             
             for i in range(self.start_backprop[1]-1):
                 next_latent = self.processor_First_Order( next_latent, dt[:,i,:], latent_boundaries[:,i,:])
@@ -199,12 +198,12 @@ class Training_Losses():
         elif self.start_backprop[0] == 2: # Encode full field self.start_backprop[1] steps in advance and from there TBPP
 
             l2_AR_1 = tc.tensor(0.0,device=self.device)
-            next_latent = self.encoder(input_encoder[:,0,...])
+            next_latent, _, _, _ = self.encoder(input_encoder[:,0,...])
             for i in range(self.start_backprop[1]-1):
                 next_latent = self.processor_First_Order(next_latent, dt[:,i,:], latent_boundaries[:,i,:])
                 l2_AR_1 += self.L2_relative_loss_general(next_latent, true_latent[:,i+1,:], True)
 
-            place_holder = self.encoder(input_encoder)
+            place_holder, _, _, _ = self.encoder(input_encoder)
 
             for i in range(self.start_backprop[1]): 
                 place_holder = self.processor_First_Order( place_holder, dt[:,i:number_of_time_steps-self.start_backprop[1]+i,:].flatten().unsqueeze(-1) , latent_boundaries[:,i:number_of_time_steps-self.start_backprop[1]+i,:].reshape(-1, latent_boundaries.size(-1)))
@@ -268,70 +267,4 @@ class Training_Losses():
             L2_relative = tc.mean(tc.linalg.vector_norm(inp - target, dim=-1) / tc.max(norm, eps))
             return L2_relative
         
-        
-    def create_padding_mask(self, size_of_tensor: list, length_of_padding: tc.tensor):
-        
-        mask = tc.zeros(size_of_tensor, device = self.device)
-        columns = tc.arange(size_of_tensor[1])
-        where_to_fill = columns>=length_of_padding
-        where_to_fill = where_to_fill[(...,) + (None,) * (len(size_of_tensor)-2)].expand(size_of_tensor).to(self.device)
-        mask = mask.masked_fill(where_to_fill, 1.0)
-        
-        return mask
-
-
-    def MSE(self, input: list, target: list, length_of_padding: tc.tensor, reconstructed_boundaries: tc.tensor = None, boundaries: tc.tensor = None, is_denormalized_validation = False):
-        mse = []
-        if tc.any(length_of_padding != 0.0):
-            for count, i in enumerate(input):   
-                if is_denormalized_validation:
-                    i = i.double()
-                    target[count] = target[count].double()
-                loss = nn.MSELoss(reduction='none')
-                element_loss = loss(i, target[count]) 
-                mask = self.create_padding_mask( size_of_tensor=i.size(), length_of_padding=length_of_padding) 
-                masked_loss = element_loss * mask
-                if is_denormalized_validation:
-                    masked_target = ((target[count] * mask)**2).mean()
-                    mse_value = (masked_loss.double()/masked_target.double()).sum() / (mask.sum() * i.size(2))
-                    mse.append(mse_value)
-                else:
-                    mse_value = masked_loss.sum() / (mask.sum() * i.size(2))
-                    mse.append(mse_value)
-        
-            if reconstructed_boundaries is not None:
-                element_loss = loss(reconstructed_boundaries, boundaries) 
-                mask = self.create_padding_mask( size_of_tensor=reconstructed_boundaries.size(), length_of_padding=length_of_padding) 
-                masked_loss = element_loss * mask
-                
-                if is_denormalized_validation:
-                    masked_target = ((boundaries * mask)**2).mean()
-                    mse_value = (masked_loss/masked_target).sum() / (mask.sum() * reconstructed_boundaries.size(2))
-                    mse.append(mse_value)
-                else:
-                    mse_value = masked_loss.sum() / (mask.sum() * reconstructed_boundaries.size(2))
-                    mse.append(mse_value)
-                    
-        else:
-            loss = nn.MSELoss()
-            for count, i in enumerate(input):
-                if is_denormalized_validation:
-                    i = i.double()
-                    target[count] = target[count].double()
-                mse_value = loss(i,target[count])
-                if is_denormalized_validation:
-                    mse_value = mse_value/(target[count]**2).mean()
-                mse.append(mse_value)
-                    
-            if reconstructed_boundaries is not None:
-                mse_value = loss(reconstructed_boundaries, boundaries)
-                if is_denormalized_validation:
-                    mse_value = mse_value/(boundaries**2).mean()
-                mse.append(mse_value) 
-                
-        if is_denormalized_validation:  
-               
-            return tc.stack(mse)
-        else:
-            return tc.stack(mse).mean()
     