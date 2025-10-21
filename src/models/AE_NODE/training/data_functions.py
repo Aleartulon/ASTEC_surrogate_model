fixed bugs to train dynamics. still need to decide how to do the actual regressive step (probably need to append always initial condition. also need to implement truncated backprop but from the beginning, not from the end. but first implement test for dynamics)

src/models/AE_NODE/training/data_functions.py
@@ -117,11 +117,11 @@ def standard_and_inverse_normalization_field(x: list, maxima_or_mean: dict, mini
 
 def create_padding_mask(size_of_tensor: list, length_of_padding: tc.tensor, device: tc.device):
         
-        mask = tc.zeros(size_of_tensor, device = device)
+        mask = tc.ones(size_of_tensor, device = device)
         columns = tc.arange(size_of_tensor[1])
         where_to_fill = columns>=length_of_padding
         where_to_fill = where_to_fill[(...,) + (None,) * (len(size_of_tensor)-2)].expand(size_of_tensor).to(device)
-        mask = mask.masked_fill(where_to_fill, 1.0)
+        mask = mask.masked_fill(where_to_fill, 0.0)
         
         return mask
 
@@ -184,14 +184,12 @@ def auto_encoding_MSE(input: list, target: list, length_of_padding: tc.tensor = 
     return mse.mean(), mse_per_variable
 
 def dynamics_MSE(input: tc.tensor, target: tc.tensor, length_of_padding: tc.tensor):
-    print(length_of_padding)
     device = input[0].device
     loss_no_reduction = nn.MSELoss(reduction='none')
     loss = nn.MSELoss()
-        
+    length_of_padding = tc.tensor([[197.]])
     if tc.any(length_of_padding != 0.0):
         mse = tc.tensor([], device = device)
-        size = i.size() 
         element_loss = loss_no_reduction(input, target) 
         mask = create_padding_mask( size_of_tensor=input.size(), length_of_padding=length_of_padding, device = device).bool()
         masked_loss = element_loss[mask] #flattened vector of values not masked

src/models/AE_NODE/training/method_functions.py
@@ -78,7 +78,6 @@ class Training_Losses():
         else:
             e2_latent_TF = self.processor_First_Order(input_processor, dt, latent_boundaries)
             e2_latent_TF = e2_latent_TF.reshape(number_batches, (number_of_time_steps-1), latent_dim)
-            
             l2_TF = dynamics_MSE(e2_latent_TF, latent_vector_fields[:, 1:, :], F.relu((length_of_padding-1))) * loss_coeff[1]
         
         if loss_coeff[3] <= 0:
@@ -95,87 +94,54 @@ class Training_Losses():
                 l2_AR = tc.tensor(0.0)
                 l_final = tc.tensor(0.0)
         else:
-            l2_AR, l_final = self.advance_from_ic(fields, latent_vector_fields, tc.reshape(dt,(number_batches,number_of_time_steps-1)).unsqueeze(-1), latent_boundaries.reshape(number_batches , (number_of_time_steps-1) , latent_boundaries.size(-1)), loss_coeff[2], train)
+            l2_AR, l_final = self.advance_from_ic(fields, latent_vector_fields, tc.reshape(dt,(number_batches,number_of_time_steps-1)).unsqueeze(-1), latent_boundaries.reshape(number_batches , (number_of_time_steps-1) , latent_boundaries.size(-1)), length_of_padding, loss_coeff[2], train)
             
         return l2_TF, l2_AR, l3, l_final
              
 
-    def advance_from_ic(self, fields:list, true_latent:tc.tensor, dt:tc.tensor, latent_boundaries:tc.tensor, coeff:list, train:bool):
-        
-        """ this function advances autoregressively the reduced vector at time t = 0 across the whole time series, effectively mimicking what happens at testing time. It is thus used to compute
-            L_2^A. We use this function to compute L_2^A and to get the full predicted time series of solution fields (needed during validation to compute a validation metric).
-            This function can implement 3 different algorithms depending on the value of self.start_backprop[0]:
-            - self.start_backprop[0] = 0: the initial confition is encoded and the latent vectors are predicted autoregressively keeping the gradients since the encoding
-            - self.start_backprop[0] = 1: the initial confition is encoded and the latent vectors are predicted autoregressively keeping the gradients since up to the previous self.start_backprop[1] time steps
-            - self.start_backprop[0] = 2: the initial confition is encoded and the latent vectors are predicted autoregressively keeping the gradients since up to the previous self.start_backprop[1] time steps,
-            where self.start_backprop[1]=1 in the first epoch and is then increased by 1 dynamically every TBPP_dynamic[1] epochs, where TBPP_dynamic is specified in initial_information.yaml.
-
-        Args:
-            self.encoder (class 'src.architecture.self.encoder): self.encoder
-            self.f (src.architecture.F_Latent): function self.f of the ODE of the latent dynamics
-            self.decoder (src.architecture.self.decoder): self.decoder
-            input_encoder (torch.Tensor): tensor of dimensions [B,T,C,x_dim_1, dim_x_1, dim_x_2, ...], where B is batch_size, T is the length of the full time series, C is the number of channels of the 
-                predicted solution field, dim_x_1, dim_x_2, ... are the dimensions of the first spatial dimension, second spatial dimension, etc. It is the field solution over time
-            true_latent (torch.Tensor): tensor of dimensions [B,T,latent_dim], where B is batch_size, T is the length of the full time series and latent_dim is the dimension of the latent space. It is the expected latent vectors
-            dt (torch.Tensor): a tensor containing the dts used to advance each snapshot in time. It has dimensions [B, T-1], where B is the batch size and T is the length of the time series. it assumes each batch evolves accordingly to the same dts 
-            latent_boundaries (torch.Tensor): tensor of dimension [B, T, num_params], where B is the batch size and T is the length of the time series and num_params the number of parameters of the system
-            self.k (int): stage of Runge-Kutta algorithm
-            self.RK (dict): dictionary with Butcher tablue for Runge-Kutta algorithms
-            self.device (torch.self.device): self.device where the training and validation are done
-            self.start_backprop (list): list with values that determine up to which time-step in the past backpropagate the gradients
-            size (torch.Size): tensor representing the length of each dimension of the solution field tensor
-            coeff (float): coefficient that multiplies the loss function L_2^A
-            dim_input (list): first dimension is the channels of the solution field, second is the number of spatial dimensions
-            train (bool): if true, this function was called inside the training loop, otherwise in the validation loop
-            time_dependence_in_f (bool):  if true, the function self.f depends on time as well.
-
-        Returns:
-            torch.tensor(), torch.tensor():  the mean L_2^A,  the loss function which takes into account the field solution predicted autoregressively (only at validation)
-        """    
-        number_batches = fields[0].size(0)
+    def advance_from_ic(self, fields:list, true_latent:tc.tensor, dt:tc.tensor, latent_boundaries:tc.tensor, length_of_padding: tc.tensor, loss_coeff:float, train:bool):
+        which_technique = self.autoregressive_step['which_technique']
         number_of_time_steps = fields[0].size(1)
         initial_condition = [tensor[:, 0:1, ...] for tensor in fields]
         
         if not (self.is_coupled[0]) and (self.is_coupled[1] == 'AE'):
             return tc.tensor(0.0), tc.tensor(0.0)
             
-        if self.start_backprop[0] == 0 or (not train):  #Encode initial condition and evolve in latent
+        if which_technique == 'fully_autoregressive' or (not train):  #Encode initial condition and evolve in latent. Always done at validation to compute the actual final loss autoregressively
             loss_final = tc.tensor(0., device = self.device)
             loss_final_per_variables = tc.zeros(self.number_of_different_domains, device = self.device)
             l2_AR = tc.tensor(0., device = self.device)
 
-            if (number_of_time_steps-1-self.start_backprop[1]) == 0:
-                next_latent, _, _ , _ = self.encoder(initial_condition)
-            else:
-                with tc.no_grad():
-                    next_latent, _ , _ , _ = self.encoder(initial_condition)
-
             if (not train):
                 fields = standard_and_inverse_normalization_field(fields, self.maxima_or_mean, self.minima_or_std, self.which_normalization, True)
+                fields = [tensor[:, 1:, ...] for tensor in fields]
+                reconstructed_fields_from_latent = [tc.zeros_like(field) for field in fields]
+                
+            reconstructed_latent = tc.zeros_like(true_latent)[:,1:,:]
+                
+            next_latent, _, _ , _ = self.encoder(initial_condition)
             
-            step = 0
             for count in range(number_of_time_steps-1):
-                if count <= (number_of_time_steps-1-self.start_backprop[1]):
-                    with tc.no_grad():
-                        next_latent = self.processor_First_Order( next_latent, dt[:,count,:], latent_boundaries[:,count,:])
-                        l2_AR += dynamics_MSE(next_latent, true_latent[:,count+1,:], True) 
-                        step+=1
-                else:
-                    next_latent = self.processor_First_Order(next_latent, dt[:,count,:], latent_boundaries[:,count,:])
-                    l2_AR += dynamics_MSE(next_latent, true_latent[:,count+1,:], True) 
-                    step+=1
-                    
+                
+                next_latent = self.processor_First_Order(next_latent, dt[:,count,:], latent_boundaries[:,count,:])
+                reconstructed_latent[:,count,:] = next_latent
+                
                 if (not train):
                     output_decoder, _ = self.decoder(next_latent)
                     output_decoder = [tensor.unsqueeze(1) for tensor in output_decoder]
+        
+                    for index, field in enumerate(output_decoder):
+                        reconstructed_fields_from_latent[index][:,count:count+1,:] = field
                     
-                    denorm_latent = standard_and_inverse_normalization_field(output_decoder, self.maxima_or_mean, self.minima_or_std, self.which_normalization, True)
-                    denorm_latent = [tensor.squeeze(1) for tensor in denorm_latent]
-                    fields_at_correct_time_step = [tensor[:, count+1, ...] for tensor in fields]
-                    l_final, l_final_per_variable = auto_encoding_MSE(denorm_latent, fields_at_correct_time_step) #the boundary should not be taken into account here
-                    loss_final += l_final
-                    loss_final_per_variable += l_final_per_variable
-            return l2_AR/step * coeff, l_final/(number_of_time_steps-1)
+            l2_AR = dynamics_MSE(reconstructed_latent, true_latent[:,1:,:], True) 
+            
+            if (not train):
+                reconstructed_fields_from_latent = standard_and_inverse_normalization_field(reconstructed_fields_from_latent, self.maxima_or_mean, self.minima_or_std, self.which_normalization, True)
+                reconstructed_fields_from_latent = [tensor.squeeze(1) for tensor in reconstructed_fields_from_latent]
+                l_final, l_final_per_variable = auto_encoding_MSE(reconstructed_fields_from_latent, fields, is_denormalized_validation = True) #the boundary should not be taken into account here
+                return l2_AR * loss_coeff, (l_final, l_final_per_variable)
+            
+            return l2_AR * loss_coeff, tc.tensor(0.0)
         
         elif self.start_backprop[0] == 1: #Encode ic and evolve in latent but TBPP
 
@@ -202,7 +168,7 @@ class Training_Losses():
             place_holder = tc.reshape(place_holder,(number_batches,number_of_time_steps-self.start_backprop[1],true_latent.size()[-1]))
             l2_AR_2 += self.L2_relative_loss_general(place_holder, true_latent[:,self.start_backprop[1]:,:], True)
 
-            return (l2_AR_1 * (self.start_backprop[1]-1) +l2_AR_2 *(number_of_time_steps-self.start_backprop[1]))/(number_of_time_steps-1) * coeff, tc.tensor(0.0)
+            return (l2_AR_1 * (self.start_backprop[1]-1) +l2_AR_2 *(number_of_time_steps-self.start_backprop[1]))/(number_of_time_steps-1) * loss_coeff, tc.tensor(0.0)
             
         elif self.start_backprop[0] == 2: # Encode full field self.start_backprop[1] steps in advance and from there TBPP
 
@@ -220,7 +186,7 @@ class Training_Losses():
             place_holder = tc.reshape(place_holder,(number_batches,number_of_time_steps-self.start_backprop[1],true_latent.size()[-1]))
             l2_AR_2 = self.L2_relative_loss_general(place_holder, true_latent[:,self.start_backprop[1]:,:], True)
             
-            return (l2_AR_1 * (self.start_backprop[1]-1) +l2_AR_2 *(number_of_time_steps-self.start_backprop[1]))/(number_of_time_steps-1) * coeff, tc.tensor(0.0)
+            return (l2_AR_1 * (self.start_backprop[1]-1) +l2_AR_2 *(number_of_time_steps-self.start_backprop[1]))/(number_of_time_steps-1) * loss_coeff, tc.tensor(0.0)
             
     def processor_First_Order(self, latent_vector_fields:tc.tensor, dt:tc.tensor, latent_boudaries:tc.tensor):
         """this function implements the Runge-Kutta algorithms. First_Order refers to the fact that the ODE is a first order ODE, although higher orders would still be solved by this algorithms

src/models/AE_NODE/training/training_validation_functions.py
@@ -59,6 +59,7 @@ class Training():
         count = 0
         regularization_loss = 0
         l_real = 0
+        l_real_per_variable = np.zeros(self.number_of_different_domains-1)
         l1_loss_unnorm_per_variable = np.zeros(self.number_of_different_domains)
         l1_loss_unnorm = 0
         
@@ -76,7 +77,8 @@ class Training():
                 l1_mean_per_denormalized_per_variable = l1[3]
                 
                 loss += (l1_mean + l2_TF + l2_AR + l3).detach().cpu().item()
-                l_real +=  l_final.detach().item()
+                l_real +=  l_final[0].detach().item()
+                l_real_per_variable += (l_final[1]).detach().cpu().numpy()
                 l1_loss += (l1_mean ).detach().cpu().numpy()
                 l1_loss_per_variable += (l1_mean_per_variable).detach().cpu().numpy()
                 l1_loss_unnorm += (l1_mean_denormalized).detach().cpu().item()
@@ -87,7 +89,7 @@ class Training():
                 regularization_loss += regularization_latent.detach().cpu().item()
                 count += 1
                 
-        return l1_loss/count, l1_loss_per_variable/count, l1_loss_unnorm/count, l1_loss_unnorm_per_variable/count, l2_TF_loss/count, l2_AR_loss/count , l3_loss/count, l_real/count, regularization_loss/count , loss/count
+        return l1_loss/count, l1_loss_per_variable/count, l1_loss_unnorm/count, l1_loss_unnorm_per_variable/count, l2_TF_loss/count, l2_AR_loss/count , l3_loss/count, l_real/count, l_real_per_variable/count, regularization_loss/count , loss/count
 
 
     def training(self):
@@ -96,8 +98,7 @@ class Training():
             # create losses file
             os.makedirs(self.PATH+'/losses/',exist_ok=True)
             os.makedirs(self.PATH+'/checkpoint/',exist_ok=True)
-
-            loss_coeff_2 = -10
+            
             #start the training
 
             #check for coupled_system
@@ -125,11 +126,9 @@ class Training():
             valid_l2_AR = np.zeros(self.epochs)
             valid_l3 = np.zeros(self.epochs)
             valid_real = np.zeros(self.epochs)
+            valid_real_per_variable = np.zeros((self.epochs, self.number_of_different_domains-1))
             valid_regularization = np.zeros(self.epochs)
             valid_loss_tot = np.zeros(self.epochs)
-            
-            if self.TBPP_dynamic[0]:
-                self.start_backprop.append(1)
         
             for i in range(self.epochs):
 
@@ -141,32 +140,25 @@ class Training():
                 time1 = time.time()
                 if i < self.time_of_AE: #use only AR
                     before_training = time.time()
-                    train_l1_data, train_l1_per_variable_data, train_l2_TF_data, train_l2_AR_data, train_l3_data, train_regularization_data, train_loss_data = self.train_epoch([self.loss_coefficients[0],0,0,0])
+                    train_l1_data, train_l1_per_variable_data, train_l2_TF_data, train_l2_AR_data, train_l3_data, train_regularization_data, train_loss_data = self.train_epoch([self.loss_coefficients['AE'],0,0,0])
                     before_validation = time.time()
-                    valid_l1_data, valid_l1_per_variable_data, valid_l1_unnorm_data, valid_l1_unnorm_per_variable_data, valid_l2_TF_data, valid_l2_AR_data, valid_l3_data, valid_real_data, valid_regularization_data, valid_loss_data = self.valid_epoch([1,1,1,1])
+                    valid_l1_data, valid_l1_per_variable_data, valid_l1_unnorm_data, valid_l1_unnorm_per_variable_data, valid_l2_TF_data, valid_l2_AR_data, valid_l3_data, valid_real_data, valid_real_per_variable_data, valid_regularization_data, valid_loss_data = self.valid_epoch([1,1,1,1])
                     if self.is_coupled[0]:
                         valid_loss_data = 100.0
                 elif i >=self.time_of_AE and i < self.time_only_TF: #use only TF
                     before_training = time.time()
-                    train_l1_data, train_l1_per_variable_data, train_l2_TF_data, train_l2_AR_data, train_l3_data, train_regularization_data, train_loss_data = self.train_epoch([self.loss_coefficients[0],self.loss_coefficients[1],0, self.loss_coefficients[3]])
-                    valid_l1_data, valid_l1_per_variable_data, valid_l1_unnorm_data, valid_l1_unnorm_per_variable_data, valid_l2_TF_data, valid_l2_AR_data, valid_l3_data, valid_real_data, valid_regularization_data, valid_loss_data = self.valid_epoch([1,1,1,1])
+                    train_l1_data, train_l1_per_variable_data, train_l2_TF_data, train_l2_AR_data, train_l3_data, train_regularization_data, train_loss_data = self.train_epoch([self.loss_coefficients['AE'],self.loss_coefficients['TF'],0, self.loss_coefficients['Random_DT']])
+                    valid_l1_data, valid_l1_per_variable_data, valid_l1_unnorm_data, valid_l1_unnorm_per_variable_data, valid_l2_TF_data, valid_l2_AR_data, valid_l3_data, valid_real_data, valid_real_per_variable_data, valid_regularization_data, valid_loss_data = self.valid_epoch([1,1,1,1])
                 else:
-                    loss_coeff_2 = self.loss_coefficients[2] * self.AR_strength * full_training_count
-                    full_training_count +=1
-                    if loss_coeff_2 >= self.loss_coefficients[2]:
-                        loss_coeff_2 = self.loss_coefficients[2]
-
-                    if self.TBPP_dynamic[0]  and self.start_backprop[1] < self.TBPP_dynamic[2] and full_training_count%self.TBPP_dynamic[1] == 0:
-                        self.start_backprop[1] += 1
+                    
                     before_training = time.time()
-                    train_l1_data, train_l1_per_variable_data, train_l2_TF_data, train_l2_AR_data, train_l3_data, train_regularization_data, train_loss_data = self.train_epoch([self.loss_coefficients[0],self.loss_coefficients[1],loss_coeff_2,self.loss_coefficients[3]])
+                    train_l1_data, train_l1_per_variable_data, train_l2_TF_data, train_l2_AR_data, train_l3_data, train_regularization_data, train_loss_data = self.train_epoch([self.loss_coefficients['AE'],self.loss_coefficients['TF'],self.loss_coefficients['AR'],self.loss_coefficients['Random_DT']])
                     before_validation = time.time()
-                    valid_l1_data, valid_l1_per_variable_data, valid_l1_unnorm_data, valid_l1_unnorm_per_variable_data, valid_l2_TF_data, valid_l2_AR_data, valid_l3_data, valid_real_data, valid_regularization_data, valid_loss_data = self.valid_epoch([1,1,1,1])
+                    self.loss_coefficients['AR'] += self.loss_coefficients['AR_strength']
+                    valid_l1_data, valid_l1_per_variable_data, valid_l1_unnorm_data, valid_l1_unnorm_per_variable_data, valid_l2_TF_data, valid_l2_AR_data, valid_l3_data, valid_real_data, valid_real_per_variable_data, valid_regularization_data, valid_loss_data = self.valid_epoch([1,1,1,1])
                 
                 time2 = time.time()
                 
-                print('Time of training:', before_validation - before_training)
-                print('Time of validation:', time2 - before_validation)
                 
                 if i > self.time_of_AE:
                     self.scheduler.step()
@@ -188,6 +180,7 @@ class Training():
                 valid_l2_AR[i] = valid_l2_AR_data
                 valid_l3[i] = valid_l3_data
                 valid_real[i] = valid_real_data
+                valid_real_per_variable[i] = valid_real_per_variable_data
                 valid_regularization[i] = valid_regularization_data
                 valid_loss_tot[i] = valid_loss_data
 
@@ -212,27 +205,38 @@ class Training():
 
 
                 print("Epoch: " +str(i)+', ' + str(time2-time1)+ ' s')
-                if self.TBPP_dynamic[0]:
-                    print("self.start_backprop for TBPP: " +str(self.start_backprop[1]))
+                print('Time of training:', before_validation - before_training)
+                print('Time of validation:', time2 - before_validation)
+                
+                if self.loss_coefficients['AR'] != 0.0 and self.autoregressive_step['which_technique'] == 'TBPP_from_end':
+                    print(" for TBPP: " +str(self.autoregressive_step['TBPP_from_end_config'][0]))
+                    
+                elif self.loss_coefficients['AR'] != 0.0 and self.autoregressive_step['which_technique'] == 'TBPP_from_start':
+                    print(" for TBPP: " +str(self.autoregressive_step['TBPP_from_start_config'][0]))
+                
+                elif self.loss_coefficients['AR'] != 0.0:
+                    print("Strength of autoregressive step: ", self.loss_coefficients['AR'])
+                    
                 print('')
-                print("Loss coefficient 2: " +str(loss_coeff_2))
+                print("Loss coefficient 2: " +str(self.loss_coefficients['AR']))
                 print('Train_loss_data = ' + str(train_loss_data) + 
-                    '\nl1 train loss = ' + str(train_l1_data) + 
-                    '\n l1 train loss per variable' + str(train_l1_per_variable_data)+
-                    '\nl2 TF train loss = ' + str(train_l2_TF_data) + 
-                    '\nl2 AR train loss = ' + str(train_l2_AR_data) + 
-                    '\nl3 train loss = ' + str(train_l3_data) + 
+                    '\nAE train loss = ' + str(train_l1_data) + 
+                    '\n AE train loss per variable' + str(train_l1_per_variable_data)+
+                    '\nTF train loss = ' + str(train_l2_TF_data) + 
+                    '\nAR train loss = ' + str(train_l2_AR_data) + 
+                    '\nRandom dt train loss = ' + str(train_l3_data) + 
                     '\nregularization loss = ' + str(train_regularization_data))
                 print(' ')
                 print('Valid_loss_data = ' + str(valid_loss_data) + 
                     '\nvalid Real loss = ' + str(valid_real_data) + 
-                    '\nl1 valid loss = ' + str(valid_l1_data) + 
-                    '\n l1 valid loss per variable' + str(valid_l1_per_variable_data)+
-                    '\nl1 valid unnorm  = ' + str(valid_l1_unnorm_data) + 
-                    '\nl1 valid unnorm per variable  = ' + str(valid_l1_unnorm_per_variable_data) + 
-                    '\nl2 valid TF loss = ' + str(valid_l2_TF_data) + 
-                    '\nl2 valid AR loss = ' + str(valid_l2_AR_data) + 
-                    '\nl3 valid loss = ' + str(valid_l3_data) + 
+                    '\nvalid Real per variable loss = ' + str(valid_real_per_variable_data) + 
+                    '\nAE valid loss = ' + str(valid_l1_data) + 
+                    '\n AE valid loss per variable' + str(valid_l1_per_variable_data)+
+                    '\nAE valid unnorm  = ' + str(valid_l1_unnorm_data) + 
+                    '\nAE valid unnorm per variable  = ' + str(valid_l1_unnorm_per_variable_data) + 
+                    '\nvalid TF loss = ' + str(valid_l2_TF_data) + 
+                    '\nvalid AR loss = ' + str(valid_l2_AR_data) + 
+                    '\nRandom dt valid loss = ' + str(valid_l3_data) + 
                     '\nvalid regularization = ' + str(valid_regularization_data))
                 print('The validation loss has not decreased for ' + str(early_stopping) + ' self.epochs!')
                 
@@ -247,12 +251,12 @@ class Training():
                 if np.mean(valid_loss_data) < loss_value: #careful valid loss tot!!
                     loss_value = np.mean(valid_loss_data)
                     print('Models saved!')
-                    save_checkpoint(self.encoder, self.f , self.decoder, self.optim, self.scheduler, i, loss_value, loss_coeff_2 , self.start_backprop, full_training_count,self.PATH+'/checkpoint/check.pt')
+                    save_checkpoint(self.encoder, self.f , self.decoder, self.optim, self.scheduler, i, loss_value, self.loss_coefficients['AR'] , self.autoregressive_step, full_training_count,self.PATH+'/checkpoint/check.pt')
                     early_stopping = 0
         
         else:
 
-            self.encoder, self.f, self.decoder, self.optim, scheduler, start_epoch, loss, loss_coeff_2, self.start_backprop, full_training_count = load_checkpoint(self.encoder, self.f , self.decoder, self.optim, scheduler, self.PATH+'/checkpoint/check.pt', self.device)
+            self.encoder, self.f, self.decoder, self.optim, scheduler, start_epoch, loss, self.loss_coefficients['AR'], self.start_backprop, full_training_count = load_checkpoint(self.encoder, self.f , self.decoder, self.optim, scheduler, self.PATH+'/checkpoint/check.pt', self.device)
             self.encoder.to(self.device)
             self.f.to(self.device)
             self.decoder.to(self.device) 
@@ -295,15 +299,15 @@ class Training():
                     train_l1_data, train_l2_TF_data, train_l2_AR_data, train_l3_data, train_regularization_data, train_loss_data = train_epoch(self.encoder, self.f, self.decoder,  self.device, self.optim, training_data,[self.loss_coefficients[0],self.loss_coefficients[1],0,self.loss_coefficients[3]], self.RK, self.k, self.start_backprop, self.lambda_regularization, self.clipping, self.is_coupled)
                     valid_l1_data, valid_l1_unnorm_per_variable_data, valid_l2_TF_data, valid_l2_AR_data, valid_l3_data, valid_real_data, valid_regularization_data, valid_loss_data = valid_epoch(self.encoder, self.f, self.decoder,  self.device, validation_data,[1,1,1,1], self.RK, self.k, self.start_backprop, self.lambda_regularization, self.is_coupled)
                 else:
-                    loss_coeff_2 = self.loss_coefficients[2] * AR_strength * full_training_count
+                    self.loss_coefficients['AR'] = self.loss_coefficients[2] * AR_strength * full_training_count
                     full_training_count +=1
-                    if loss_coeff_2 >= self.loss_coefficients[2]:
-                        loss_coeff_2 = self.loss_coefficients[2]
+                    if self.loss_coefficients['AR'] >= self.loss_coefficients[2]:
+                        self.loss_coefficients['AR'] = self.loss_coefficients[2]
 
                     if self.TBPP_dynamic[0]  and self.start_backprop[1] < self.TBPP_dynamic[2] and full_training_count%self.TBPP_dynamic[1] == 0:
                         self.start_backprop[1] += 1
                         
-                    train_l1_data, train_l2_TF_data, train_l2_AR_data, train_l3_data, train_regularization_data, train_loss_data = train_epoch(self.encoder, self.f, self.decoder, self.device, self.optim, training_data,[self.loss_coefficients[0],self.loss_coefficients[1],loss_coeff_2,self.loss_coefficients[3]], self.RK, self.k, self.start_backprop,self.lambda_regularization, self.clipping, self.is_coupled)
+                    train_l1_data, train_l2_TF_data, train_l2_AR_data, train_l3_data, train_regularization_data, train_loss_data = train_epoch(self.encoder, self.f, self.decoder, self.device, self.optim, training_data,[self.loss_coefficients[0],self.loss_coefficients[1],self.loss_coefficients['AR'],self.loss_coefficients[3]], self.RK, self.k, self.start_backprop,self.lambda_regularization, self.clipping, self.is_coupled)
                     valid_l1_data, valid_l1_unnorm_per_variable_data, valid_l2_TF_data, valid_l2_AR_data, valid_l3_data, valid_real_data, valid_regularization_data, valid_loss_data = valid_epoch(self.encoder, self.f, self.decoder, self.device, validation_data,[1,1,1,1], self.RK, self.k,self.start_backprop, self.lambda_regularization, self.is_coupled)
 
                 time2 = time.time()
@@ -348,7 +352,7 @@ class Training():
                 print("Epoch: " +str(i)+', ' + str(time2-time1)+ ' s')
                 if self.TBPP_dynamic[0]:
                     print("self.start_backprop for TBPP: " +str(self.start_backprop[1]))
-                print("Loss coefficient 2: " +str(loss_coeff_2))
+                print("Loss coefficient 2: " +str(self.loss_coefficients['AR']))
                 print('Train_loss_data = ' + str(train_loss_data) + ', l1 train loss = ' +str(train_l1_data) + ', l2 TF train loss = ' + str(train_l2_TF_data)+ ', l2 AR train loss = ' + str(train_l2_AR_data)+ ', l3 train loss = ' + str(train_l3_data)+ ', regularization loss = ' + str(train_regularization_data))
                 print('Valid_loss_data = ' + str(valid_loss_data)+ ', valid Real loss = ' +str(valid_real_data)  + ', l1 valid loss = ' +str(valid_l1_data) +  ', l1 valid unnorm loss = ' +str(valid_l1_unnorm_per_variable_data) + ', l2 valid TF loss = ' + str(valid_l2_TF_data)+ ', l2 valid AR loss = ' + str(valid_l2_AR_data)+ ', l3 valid loss = ' + str(valid_l3_data)+ ', valid regularization = ' + str(valid_regularization_data))
                 print('The validation loss has not decreased for ' + str(early_stopping) + ' epochs!')
@@ -364,6 +368,6 @@ class Training():
                 if valid_loss_data < loss_value: #careful valid loss tot!!
                     loss_value = valid_loss_data
                     print('Models saved!')
-                    save_checkpoint(self.encoder, self.f , self.decoder, self.optim, scheduler, i, loss_value, loss_coeff_2 , self.start_backprop, full_training_count,self.PATH+'/checkpoint/check.pt')
+                    save_checkpoint(self.encoder, self.f , self.decoder, self.optim, scheduler, i, loss_value, self.loss_coefficients['AR'] , self.start_backprop, full_training_count,self.PATH+'/checkpoint/check.pt')
                     early_stopping = 0
 