6bed78bc55ec040900096889e17b91c4c37c6b48
Alessandro Longhi <aalelonghi@login03.delftblue.tudelft.nl>
2025-12-07 15:11:26 +0100

    before merging from main

--- src/models/AE_NODE/training/training_validation_functions.py
+++ src/models/AE_NODE/training/training_validation_functions.py
@@ -222,7 +222,7 @@ class Training():
                                                                                      self.data_training_path_dynamic, self.data_validation_path_dynamic, 
                                                                                      self.number_of_workers, self.data_path, self.where_to_save_data, 
                                                                                      self.which_normalization, self.device, 
-                                                                                     self.config_training['indeces_training_boundaries'], self.config_validation['indeces_training_boundaries'],
+                                                                                     self.config_training['indeces_training_boundaries'], self.config_training['indeces_validation_boundaries'],
                                                                                      self.all_on_gpu, self.pin_memory, self.indeces_training_boundaries, self.indeces_validation_boundaries)
                         before_next_window_change = self.waiting_epochs_before_new_dataset_creation[how_many_datasets_creations]
                         how_many_datasets_creations+=1
