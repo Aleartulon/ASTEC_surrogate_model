28eba419e04b310a81dbcb30a3cf40bb4db7c529
Alessandro Longhi <aalelonghi@apollo.tnw.tudelft.nl>
2026-02-12 13:50:17 +0100

    added histograms of errors and in the process of fixing smoothing

--- src/models/AE_NODE/testing/support_functions.py
+++ src/models/AE_NODE/testing/support_functions.py
@@ -260,7 +260,13 @@ def compute_global_errors(where_to_get_data:str, where_to_save_data: str, genera
             statistics_errors[metric][name].append(std)
             
     for metric in dictionary_of_errors:  
-        write_dict_to_txt(statistics_errors[metric], f"{where_to_save_data}/{metric}.txt", len(txt_files), txt_files)
+        write_dict_to_txt(statistics_errors[metric], f"{where_to_save_data}/{metric}.txt")
+    
+    write_info(f"{where_to_save_data}/info.txt", len(txt_files), txt_files)
+        
+    #generate plots with aggregated images
+    for metric in dictionary_of_errors:  
+        plot_aggregated_errors_per_variable(f"{where_to_save_data}/{metric}.txt", f"{where_to_save_data}",f"{metric}")
 
     # generate instograms
     if generate_istograms:
@@ -273,23 +279,102 @@ def compute_global_errors(where_to_get_data:str, where_to_save_data: str, genera
                 plt.title(f"{variable.replace('_', ' ')}, {which_prediction} prediction", fontsize = 16)
                 plt.savefig(f'{where_to_save_data}/hist_{variable}_{metric}.png', dpi=300, bbox_inches='tight')
                 plt.close()
+                
+    
     return 0
 
-def write_dict_to_txt(data_dict, output_file, how_many_trajectories: int, which_trajectories: list):
+def write_dict_to_txt(data_dict, output_file):
 
     with open(output_file, 'w') as f:
         # Write header
-        f.write(f"{'Variable name':<40} {'Mean':<20} {'Std':<20}\n")
+        f.write(f"{'Variable name':<44} {'Mean':<20} {'Std':<20}\n")
         f.write("-" * 80 + "\n")
         
         # Write data rows
         for var_name, values in data_dict.items():
             mean = values[0]
             std = values[1]
-            f.write(f"{var_name:<40} {mean:<20.10e} {std:<20.10e}\n")
+            f.write(f"{var_name:<44} {mean:<20.10e} {std:<20.10e}\n")
         f.write("-" * 80 + "\n")
+
+def write_info( output_file, how_many_trajectories: int, which_trajectories: list):
+
+    with open(output_file, 'w') as f:
         f.write(f"{how_many_trajectories} total trajectories for testing \n")
         f.write(f"Files used:\n")
         for i in which_trajectories:
             f.write(f"{i}\n")
+            
+def plot_aggregated_errors_per_variable(path_txt_file:str, saving_path:str, metric:str):
+    df = pd.read_fwf(path_txt_file, skiprows=2, skipfooter=1, engine='python',
+                  colspecs=[(0, 45), (45, 64), (64, 85)],
+                  names=['Variable', 'Mean', 'Std'])
+    dict_global = {}
+    dict_vessel = {}
+    dict_plenum = {}
+    dict_core = {}
+    dict_faces = {}
+    
+    for count, i in enumerate(df['Variable']):
+        split = i.rsplit('_', 1)
+        name = split[0].replace('_', ' ') 
+        if split[-1] == 'scalar':
+            dict_global[name] = [df['Mean'][count],df['Std'][count]]
+        elif split[-1] == 'vessel':
+            dict_vessel[name] = [df['Mean'][count],df['Std'][count]]
+        elif split[-1] == 'core':
+            dict_core[name] = [df['Mean'][count],df['Std'][count]]
+        elif split[-1] == 'faces':
+            dict_faces[name] = [df['Mean'][count],df['Std'][count]]
+        elif split[-1] == 'plenum':
+            dict_plenum[name] = [df['Mean'][count],df['Std'][count]]
+        else:
+            raise TypeError('Something wrong')
+    
+    
+        
+    make_hist_and_plots(dict_global, saving_path ,metric, latex_metric = which_metric(metric, "g"), label = 'g')
+    make_hist_and_plots(dict_vessel, saving_path ,metric,  latex_metric = which_metric(metric, "v"), label = 'v')
+    make_hist_and_plots(dict_plenum, saving_path ,metric,  latex_metric = which_metric(metric, "p"), label = 'p')
+    make_hist_and_plots(dict_core, saving_path ,metric, latex_metric = which_metric(metric, "cr"), label = 'cr')
+    make_hist_and_plots(dict_faces, saving_path ,metric, latex_metric = which_metric(metric, "f"), label = 'f')
+    
+    return 0
+
+def make_hist_and_plots(data_dictionary:dict, path:str,metric:str, latex_metric:str, label:str):
+    
+    #make histograms
+    
+    hist_array = [data_dictionary[x][0] for x in data_dictionary]
+    fig, axs = plt.subplots(1, 1, sharey=True, tight_layout=True)
+    axs.hist(hist_array, bins=np.logspace(np.log10(min(hist_array)), np.log10(max(hist_array)), 100))
+    axs.set_xscale('log')
+    axs.set_xlabel(latex_metric, fontsize = 16)
+    axs.set_ylabel('Frequency', fontsize = 16)
+    median_val = np.median(hist_array)
+    axs.axvline(median_val, color='red', linestyle='--', label=f'Median: {median_val:.2e}')
+    plt.title(rf'{latex_metric} distribution for $s_{{{label}}}$ variables', fontsize=16)
+    os.makedirs(f'{path}/{metric}/', exist_ok=True)
+    plt.savefig(f'{path}/{metric}/{metric}_{label}_histogram.png', dpi=300, bbox_inches='tight')
+    plt.close(fig)
+    
+    #make plots
+    
+def which_metric(metric, variable):
+    if metric == "RMSE":
+        return rf"$RMSE_{{{variable}}}$"
+    elif metric == "RMSE_divided_by_mean":
+        return rf"$RMSE_{{{variable}, mean}}$"
+    elif metric == "RMSE_divided_by_std":
+        return rf"$RMSE_{{{variable}, std}}$"
+    elif metric == "RMSE_divided_by_max":
+        return rf"$RMSE_{{{variable}, max}}$"
+    elif metric == "MSE":
+        return rf"$MSE_{{{variable}}}$"
+    elif metric == "L2_error_norm":
+        return rf"$L2_error_norm_{{{variable}}}$"
+    elif metric == "MSE_normalized_by_mean":
+        return rf"$MSE normalized by mean_{{{variable}}}$"
+    else:
+        raise TypeError(f"Something wrong, metric is {metric}")
     
